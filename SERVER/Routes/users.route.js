4dc36a928fa15d9af3cebc6da5b51d5a362ec515
DianeNkz <nikuzediane11@gmail.com>
2019-11-20 13:01:15 -1200

    ft-169864395: add path to create a new case endpoint

--- SERVER/Routes/users.route.js
+++ SERVER/Routes/users.route.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import addUser from '../Controllers/users.controller';
 // import login from '../Controllers/login.controller';
-// import casefunction from "../Controllers/caseRecord";
+ import casefunction from "../Controllers/caseRecord";
 //const User= require("../Models/users");
 
 
@@ -9,6 +9,6 @@ const router= express.Router();
 
 router.post('/api/V1/auth/userSigningup', addUser);
 //router.post('/api/V1/auth/userLogging/', login);
-//router.post('/api/V1/case/newIncident/', casefunction.createCase);
+router.post('/api/V1/case/newIncident/', casefunction.createCase);
 
 export default router ;
