f8494be7955851428bda9e6842f229b381285f3a
DianeNkz <nikuzediane11@gmail.com>
2019-11-21 13:02:21 -1200

    ft-169867137: add fetch all cases api endpoint
    add route to fetch all cases api endpoint
    fetch all created cases api endpoint

--- SERVER/Controllers/caseRecord.js
+++ SERVER/Controllers/caseRecord.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import cases from '../Models/cases';
+import validateCase from '../Controllers/validateCase';
 
 
  class casefunction {
      createCase (req,res)  {
-        const { error } = (req.body);
+        const { error } = validateCase(req.body);
 
         if(error) return res.status(400).send(error.details[0].message);
 
@@ -27,10 +28,44 @@ import cases from '../Models/cases';
         res.json({
             status: 201,
             message: 'A case created successfully',
+            data:cases
         });
     }
 
+    // updateCase(req,res) {
+    //     const { error } = validateCase(req.body);
 
+    //     if(error) return res.status(400).send(error.details[0].message);
+        
+    //     const casescreated= cases.find(c => c.id === parseInt(req.params.id));
+    //     if(!casescreated) return res.status(404).send('The given id is not found');
+
+    //     c.title= req.body.title;
+    //     c.description = req.body.description;
+
+    //     if(error) return res.status(400).send(error.details[0].message);
+
+    //     res.status(200).json(
+    //         {
+    //             status: 200, 
+    //             message: 'Article updated succesfully'
+    //          });
+    
+    // }
+
+
+    allCases (req,res){
+        const { error } = (req.body);
+        if(error) {
+            res.status(400).json({
+                status: 400,
+                error: error.details[0].message
+            });
+            return ;
+        }
+
+        res.send(cases);
+    }
 
  }
 
--- SERVER/Routes/users.route.js
+++ SERVER/Routes/users.route.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import addUser from '../Controllers/users.controller';
-// import login from '../Controllers/login.controller';
- import casefunction from "../Controllers/caseRecord";
+//import login from '../Controllers/login.controller';
+import casefunction from "../Controllers/caseRecord";
 //const User= require("../Models/users");
 
 
@@ -10,5 +10,7 @@ const router= express.Router();
 router.post('/api/V1/auth/userSigningup', addUser);
 //router.post('/api/V1/auth/userLogging/', login);
 router.post('/api/V1/case/newIncident/', casefunction.createCase);
+//router.post('/api/V1/case/updateIncident/', casefunction.updateCase);
+router.get('/api/V1/case/allIncident/', casefunction.allCases);
 
 export default router ;
