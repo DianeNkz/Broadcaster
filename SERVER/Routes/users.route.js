0cfe94ab312b9da9b04c6a211f0868d5c5294307
DianeNkz <nikuzediane11@gmail.com>
2019-11-28 13:11:18 -1200

    ft-169916190: test apis
    add test for creating a user api endpoint
    add test for login api endpoint
    add test for getting all created cases
    
    close [#169916190]

--- SERVER/Routes/users.route.js
+++ SERVER/Routes/users.route.js
@@ -3,7 +3,7 @@ import addUser from '../Controllers/users.controller';
 import login from '../Controllers/login.controller';
 import casefunction from "../Controllers/caseRecord";
 import manageAll from "../Controllers/admin.controller";
-
+//import auth from '../middleware/auth';
 
 
 
@@ -11,13 +11,11 @@ const router= express.Router();
 
 router.post('/api/V1/auth/userSignup', addUser);
 router.post('/api/V1/auth/userLogin/', login);
-router.post('/api/V1/case/newIncident/', casefunction.createCase);
+router.post('/api/V1/case/newIncident/',  casefunction.createCase);
 router.get('/api/V1/case/allIncident/', casefunction.allCases);
 router.get('/api/V1/case/specificIncident/:id', casefunction.specificCase);
 router.delete('/api/V1/case/deleteIncident/:id', casefunction.deleteCase);
 router.put('/api/V1/case/editContent/:id', casefunction.editContent);
-
-
 router.put('/api/V1/admin/editstatus/:id', manageAll.editStatus);
 
 
